c1f3eba0e3baa2b47be2c3ba2a27b240a67f6662
chema <josemanuel.carrasco.lopez@gmail.com>
2023-12-11 22:52:06 +0100

    Actividad Final M1

--- jquery/script.js
+++ jquery/script.js
@@ -24,7 +24,8 @@ $("#datosCliente").on("submit", function(event) {
     clearAlert();
     $("#labelNombre").removeClass("mandatoryError");
     $("#labelApellidos").removeClass("mandatoryError");
-
+    $("#labelSize").removeClass("mandatoryError");
+    
     let stop = false;
     if ($("#nombre").val() === ""){
         $("#labelNombre").addClass("mandatoryError");
@@ -35,11 +36,56 @@ $("#datosCliente").on("submit", function(event) {
         stop = true;
     }
 
+    let colorLetra = $('#resultado_tamano').css("color");
+    if (colorLetra === 'rgb(255, 0, 0)' || colorLetra === 'red') {
+        $('#labelSize').addClass("mandatoryError");
+        stop = true;
+    }
+
+
     if (stop) {
         showAlert("Por favor, revise los datos marcados en rojo");
         // Con "event.preventDefault()" y "return false" se consigue parar la ejecución del formulario
         // dejo el "return false" por ser algo específico de jquery
-        // event.preventDefault();
+        //event.preventDefault();
         return false;
     }
+});
+
+function checkSize() {
+    $('#resultado_tamano').text("");
+    $('#resultado_tamano').removeClass("mandatoryError");
+    
+    let size = $('#size').val(); 
+    $.ajax({
+        type: 'POST',
+        url: completeBackPath("checksize"),
+        data: {size : size},
+        success: function(response) {
+            if (response == "true") {
+                $('#resultado_tamano').text("Disponible");
+                resolve(true);
+            } else {
+                $('#resultado_tamano').addClass("mandatoryError");
+                $('#resultado_tamano').text("No Disponible");
+                resolve(false);
+            }            
+        },
+        error: function(error) {
+            console.log(error);
+            $('#resultado_tamano').addClass("mandatoryError");
+            $('#resultado_tamano').text("Error al verificar el tamaño");
+            reject(error);
+        }
+    });
+}
+
+$("#size").on('change', function() {
+    console.log(this.value);
+    checkSize();
+});
+   
+$("#size").ready(function() {
+    console.log(this.value);
+    checkSize();
 });
